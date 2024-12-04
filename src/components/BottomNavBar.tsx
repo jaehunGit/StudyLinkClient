caf3d7b73d560a0e27aabebd77f31a03a298f28d
PC-004\Administrator <ghty6323@gmail.com>
2024-12-04 10:57:07 +0900

    하단 네비게이션 바ui 변경 테두리 둥글게

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -3,21 +3,51 @@ import { View, Text, TouchableOpacity } from "react-native";
 
 const BottomNavBar = ({ activeTab, setActiveTab }: { activeTab: string; setActiveTab: (tab: string) => void }) => {
   return (
-    <View className="flex-row justify-around items-center bg-white h-16 border-t border-gray-200">
+    <View className="absolute bottom-0 left-0 right-0 flex-row justify-around items-center bg-white h-16 border-t border-gray-200 rounded-t-3xl shadow-lg">
       <TouchableOpacity onPress={() => setActiveTab("Home")} className="items-center">
-        <Text className={`text-sm ${activeTab === "Home" ? "text-primary" : "text-gray-400"}`}>홈</Text>
+        <Text
+          className={`text-sm ${
+            activeTab === "Home" ? "text-black font-bold" : "text-gray-400"
+          }`}
+        >
+          홈
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => setActiveTab("Team")} className="items-center">
-        <Text className={`text-sm ${activeTab === "Team" ? "text-primary" : "text-gray-400"}`}>팀원 찾기</Text>
+        <Text
+          className={`text-sm ${
+            activeTab === "Team" ? "text-black font-bold" : "text-gray-400"
+          }`}
+        >
+          팀원 찾기
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => setActiveTab("Stats")} className="items-center">
-        <Text className={`text-sm ${activeTab === "Stats" ? "text-primary" : "text-gray-400"}`}>통계</Text>
+        <Text
+          className={`text-sm ${
+            activeTab === "Stats" ? "text-black font-bold" : "text-gray-400"
+          }`}
+        >
+          통계
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => setActiveTab("AllPosts")} className="items-center">
-        <Text className={`text-sm ${activeTab === "AllPosts" ? "text-primary" : "text-gray-400"}`}>전체</Text>
+        <Text
+          className={`text-sm ${
+            activeTab === "AllPosts" ? "text-black font-bold" : "text-gray-400"
+          }`}
+        >
+          전체
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => setActiveTab("Settings")} className="items-center">
-        <Text className={`text-sm ${activeTab === "Settings" ? "text-primary" : "text-gray-400"}`}>설정</Text>
+        <Text
+          className={`text-sm ${
+            activeTab === "Settings" ? "text-black font-bold" : "text-gray-400"
+          }`}
+        >
+          설정
+        </Text>
       </TouchableOpacity>
     </View>
   );
