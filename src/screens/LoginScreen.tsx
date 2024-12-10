26c7a4d5544fc8d199b4a2c3b980f9eb6eecd70e
PC-003\Administrator <ghty6323@gmail.com>
2024-12-10 09:38:12 +0900

    로그인 로직 변경 추후 서버 연결 및 카카오 및 구글 이미지 추가

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,29 @@
-import React from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image  } from "react-native";
 
 const LoginScreen = ({ onLogin }: { onLogin: () => void }) => {
+  const [username, setUsername] = useState(""); // 아이디 입력값 상태
+  const [password, setPassword] = useState(""); // 비밀번호 입력값 상태
+
+  const handleLogin = () => {
+    if (username.trim() !== "" && password.trim() !== "") {
+      console.log("아이디:", username);
+      console.log("비밀번호:", password);
+      onLogin();
+      // 이후 서버 연결시 수정
+    } else {
+      console.log("아이디와 비밀번호를 모두 입력해주세요.");
+    }
+  };
+
+  const handleKakaoLogin = () => {
+    console.log("카카오");
+  };
+
+  const handleGoogleLogin = () => {
+    console.log("구글");
+  };
+
   return (
     <View className="flex-1 justify-center items-center bg-background px-6">
       {/* 로고 */}
@@ -14,6 +36,8 @@ const LoginScreen = ({ onLogin }: { onLogin: () => void }) => {
       <TextInput
         className="w-full h-12 border border-gray-300 rounded-lg px-3 mb-4"
         placeholder="아이디"
+        value={username}
+        onChangeText={setUsername}
       />
 
       {/* 비밀번호 입력 */}
@@ -21,12 +45,14 @@ const LoginScreen = ({ onLogin }: { onLogin: () => void }) => {
         className="w-full h-12 border border-gray-300 rounded-lg px-3 mb-6"
         placeholder="비밀번호"
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
       />
 
       {/* 로그인 버튼 */}
       <TouchableOpacity
         className="w-full h-12 bg-primary rounded-lg justify-center items-center"
-        onPress={onLogin}
+        onPress={handleLogin}
       >
         <Text className="text-background text-lg font-bold">로그인</Text>
       </TouchableOpacity>
@@ -35,6 +61,25 @@ const LoginScreen = ({ onLogin }: { onLogin: () => void }) => {
       <TouchableOpacity className="mt-4">
         <Text className="text-primary">회원가입하기</Text>
       </TouchableOpacity>
+
+      {/* 소셜 로그인 버튼 */}
+      <View className="mt-6 flex-row justify-between">
+        {/* 카카오 로그인 */}
+        <TouchableOpacity onPress={handleKakaoLogin} className="mr-4">
+          <Image
+            source={require("../assets/images/kakao_icon.png")}
+            style={{ width: 50, height: 50 }}
+          />
+        </TouchableOpacity>
+
+        {/* 구글 로그인 */}
+        <TouchableOpacity onPress={handleGoogleLogin}>
+          <Image
+            source={require("../assets/images/google_icon.png")}
+            style={{ width: 50, height: 50 }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
