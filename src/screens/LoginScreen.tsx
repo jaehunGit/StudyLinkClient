aa1992abf202fe4a35726f7e6ebac52acb6d02d7
PC-003\Administrator <ghty6323@gmail.com>
2024-12-13 09:53:32 +0900

    에러 메세지 수정 밑 에러메세지 표시 하단으로 변경

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -12,7 +12,7 @@ const LoginScreen = ({
 }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState<string | null>(null); // 에러 메시지 상태 추가
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleLogin = async () => {
     if (username.trim() === "" || password.trim() === "") {
@@ -24,7 +24,8 @@ const LoginScreen = ({
       const response = await axios.put(`${config.apiUrl}/user/login`, {
         userId: username,
         userPassword: password,
-      });
+      },
+      { timeout: 5000 });
 
       if (response.data.success) {
         console.log("로그인 성공:", response.data);
@@ -35,7 +36,8 @@ const LoginScreen = ({
         setErrorMessage(response.data.message || "아이디 또는 비밀번호를 확인해주세요.");
       }
     } catch (error) {
-      console.error("로그인 요청 중 오류 발생:", error);
+      console.error("로그인 요청 중 오류 발생1:", error);
+      console.error("에러 전체 정보:", JSON.stringify(error, null, 2));
       setErrorMessage("로그인 요청 중 문제가 발생했습니다. 다시 시도해주세요.");
     }
   };
@@ -47,11 +49,6 @@ const LoginScreen = ({
       <Text className="text-textSecondary text-base mb-6">
         당신의 팀원을 쉽게 찾으세요
       </Text>
-
-      {/* 에러 메시지 표시 */}
-      {errorMessage && (
-        <Text className="text-red-500 text-sm mb-4">{errorMessage}</Text>
-      )}
 
       {/* 아이디 입력 */}
       <TextInput
@@ -82,6 +79,11 @@ const LoginScreen = ({
       <TouchableOpacity className="mt-4" onPress={goToSignUp}>
         <Text className="text-primary">회원가입하기</Text>
       </TouchableOpacity>
+
+      {/* 에러 메시지 표시 */}
+      {errorMessage && (
+        <Text className="text-red-500 text-sm mt-4">{errorMessage}</Text>
+      )}
     </View>
   );
 };
